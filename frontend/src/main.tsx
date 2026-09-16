4a468b349b1c35194e85344ba516dc59fcbce4ff
bolph71656-ai <bolph71656@gmail.com>
2026-09-16 11:47:17 +0900

    Render measurement sessions panel

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { ComparisonReportPanel } from './ComparisonReports'
+import { MeasurementSessionsPanel } from './MeasurementSessions'
 import { PlacementOverviewPanel } from './PlacementOverview'
 import { RewReadonlyPanel } from './RewReadonly'
 import './styles.css'
@@ -9,6 +10,7 @@ import './styles.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <MeasurementSessionsPanel />
     <ComparisonReportPanel />
     <PlacementOverviewPanel />
     <RewReadonlyPanel />
